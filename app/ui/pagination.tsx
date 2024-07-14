23a195a05f19518558cefc1e5c9a68b085994dd0
JM Acera <john.acera@sportality.se>
2024-07-14 16:47:37 +0800

    fix more lint issues

--- app/ui/pagination.tsx
+++ app/ui/pagination.tsx
@@ -7,7 +7,7 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  const searchParams = useSearchParams();
+  const searchParams: any = useSearchParams();
   const pathname = usePathname();
   const currentPage = Number(searchParams?.get('page') || 1);
 
